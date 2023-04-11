daac1349aa580f6e0e0139ccd3d3c112c69b30d2
wyhwong <wyhwong@link.cuhk.edu.hk>
2023-04-12 00:27:15 +0800

    Development commit

---
+++ utils/binaries.py
@@ -0,0 +1,97 @@
+import numpy as np
+import surfinBH
+from numpy.random import uniform, rand
+from scipy import constants
+
+from .logger import get_logger
+
+LOGGER = get_logger(logger_name="Utils | Merger")
+
+
+def sph2cart(theta: float, phi: float) -> np.ndarray:
+    newUnitVector = [np.sin(theta) * np.cos(phi), np.sin(theta) * np.sin(phi), np.cos(theta)]
+    LOGGER.debug(
+        f"Converted unit vector in spherical coordinate to cartesian coordinate: [1, {theta}, {phi}] -> {newUnitVector}"
+    )
+    return np.array(newUnitVector, dtype=float)
+
+
+def generate_parameter(para_domain: dict) -> float:
+    return uniform(low=para_domain["min"], high=para_domain["max"])
+
+
+def load_fits(fits_name: str):
+    LOGGER.info(f"Loading surfinBH {fits_name=}, description: {surfinBH.fits_collection[fits_name].desc}.")
+    return surfinBH.LoadFits(fits_name)
+
+
+class Binary:
+    def __init__(self, fits, mass_ratio: float, chi1: np.ndarray, chi2: np.ndarray) -> None:
+        LOGGER.debug(f"Initializing merger with {mass_ratio=}, {chi1=}, {chi2}")
+        self.fits = fits
+        self.mass_ratio = mass_ratio
+        self.chi1, self.chi2 = chi1, chi2
+        LOGGER.debug(f"Initialized merger.")
+
+    # Output results: list
+    # merger_params: [mass_ratio, spin_1, spin_2, remnant_mass, error, remnant_spin, error, remnant_speed, error]
+    def merge(self) -> list:
+        merger_params = [self.mass_ratio, np.dot(self.chi1, self.chi1), np.dot(self.chi2, self.chi2)]
+        LOGGER.debug("Generating the remnant parameters for a merger.")
+        for para in ["mf", "chif", "vf"]:
+            remnant_para, err = getattr(self.fits, para)(self.mass_ratio, self.chi1, self.chi2)
+            if para == "mf":
+                LOGGER.debug(f"Computed remaining mass percentage: {remnant_para}%")
+            elif para == "chif":
+                LOGGER.debug(f"Computed remnant spin: {remnant_para}")
+                remnant_para = np.sqrt(np.dot(remnant_para, remnant_para))
+                err = np.sqrt(np.dot(err, err))
+            elif para == "vf":
+                LOGGER.debug(f"Computed remnant speed: {remnant_para} km/s")
+                remnant_para = np.sqrt(np.dot(remnant_para, remnant_para)) * constants.speed_of_light / 1000.0
+                err = np.sqrt(np.dot(err, err)) * constants.speed_of_light / 1000.0
+            merger_params += [remnant_para, err]
+        LOGGER.info(f"Generated the remnant parameters for a merger: {merger_params}")
+        return merger_params
+
+
+class RandomBinaryGenerator:
+    def __init__(self, config: dict) -> None:
+        LOGGER.debug(f"Initializing a random binary generator from config: {config}...")
+        self.config = config
+        self.config["phi"]["min"] *= np.pi
+        self.config["phi"]["max"] *= np.pi
+        self.config["theta"]["min"] *= np.pi
+        self.config["theta"]["max"] *= np.pi
+        LOGGER.info("Initialized random binary generator.")
+
+    def __call__(self) -> tuple:
+        # Conversion:
+        #   1. Heavier black hole
+        #   2. Lighter black hole
+        if self.config["massInjection"]:
+            mass_ratio = 0
+            while not (self.config["massRatio"]["min"] <= mass_ratio <= self.config["massRatio"]["max"]):
+                mass1, mass2 = generate_parameter(self.config["mass"]), generate_parameter(self.config["mass"])
+                mass1, mass2 = max(mass1, mass2), min(mass1, mass2)
+                mass_ratio = mass1 / mass2
+        else:
+            mass1, mass2 = None, None
+            mass_ratio = generate_parameter(self.config["massRatio"])
+        chi1, chi2 = self.gen_2pin()
+        LOGGER.debug(f"Output binary setting: {mass_ratio=}, {chi1=}, {chi2=}.")
+        return (mass_ratio, chi1, chi2, mass1, mass2)
+
+    def gen_spin(self) -> list:
+        spins = []
+        for _ in range(2):
+            spin = generate_parameter(self.config["spin"])
+            if self.config["align"]:
+                phi = 0.0
+                theta = 0.0 + np.round(rand()) * np.pi
+            else:
+                phi = generate_parameter(self.config["phi"])
+                theta = generate_parameter(self.config["theta"])
+            univ = sph2cart(theta, phi)
+            spins.append(spin * univ)
+        return spins
---
+++ utils/prior_simulation.py
@@ -0,0 +1,66 @@
+import pandas as pd
+from concurrent.futures import ProcessPoolExecutor, wait
+
+from .logger import get_logger
+from .common import get_prior_config, save_dict_as_yml
+from .binaries import RandomBinaryGenerator, load_fits, Binary
+
+LOGGER = get_logger(logger_name="Utils | Prior Simulation")
+
+
+def simulate_binary(binary_marker: str, binary_params: tuple) -> list:
+    massRatio, chi1, chi2, mass1, mass2 = binary_params
+    LOGGER.info(f"Simulating binary {binary_marker}, with {massRatio=}, {chi1=}, {chi2=}")
+    binary = Binary(fits=FITS, massRatio=massRatio, chi1=chi1, chi2=chi2)
+    remnant_params = binary.merge()
+    LOGGER.info(f"Simulated binary {binary_marker}, {remnant_params=}")
+    return [mass1, mass2] + remnant_params
+
+
+# This function simulates a certain number of binaries according to the prior config in config/prior_config.yml.
+# It outputs the prior as a .csv file in the output user defined directory.
+def simulate_binaries(output_dir) -> list:
+    global FITS
+    LOGGER.debug("Getting prior by simulation...")
+
+    # Get prior config and save in output directory
+    prior_config = get_prior_config()
+    save_dict_as_yml(path=f"{output_dir}/prior_config.yml", input_dict=prior_config)
+    merger_config = prior_config["merger"]
+    binary_generator = RandomBinaryGenerator(config=merger_config)
+    num_binaries = prior_config["amount"]
+    FITS = load_fits(fits_name=prior_config["fits"])
+    futures, prior = [], []
+
+    # Simulate the mergers with the generated binary configs using multi-processing
+    with ProcessPoolExecutor(max_workers=20) as Executor:
+        for binary_marker in range(num_binaries):
+            binary_params = binary_generator()
+            futures.append(Executor.submit(simulate_binary, binary_marker, binary_params))
+    wait(futures)
+
+    # Process the results to a dataframe
+    for _, remnantParams in enumerate(futures):
+        prior.append(remnantParams.result())
+    prior = pd.DataFrame(
+        prior, columns=["m1", "m2", "mr", "chi1", "chi2", "mf", "mfError", "chif", "chifError", "vf", "vfError"]
+    )
+
+    # TODO: Some descriptive visualization of the prior
+    # dataset = prior.drop(columns=["m1", "m2", "mr", "chi1", "chi2", "mfError", "chifError", "vfError"])
+    # dataset.columns = ["$m_f$", "$\chi_f$", "$v_f$"]
+    # plot_distribution(dataset=dataset, output_dir=output_dir, savefig=True)
+    # LOGGER.info("Plotted distribution of simulated parameters.")
+    # plot_kick_against_spin(dataset=dataset, output_dir=output_dir, savefig=True)
+    # LOGGER.info("Plotted correlation of kick and spin.")
+    # plot_kick_spin_correlation(dataset=dataset, nbins=10, maxSpin=1.0, minSpin=0.0, output_dir=output_dir, savefig=True)
+    # LOGGER.info("Plotted correlation of kick and spin.")
+
+    # Remove mass_1 and mass_2 if mass injection is not on
+    if not prior_config["merger"]["massInjection"]:
+        prior = prior.drop(columns=["m1", "m2"])
+
+    # Save the prior as a csv file
+    prior.to_csv(f"{output_dir}/prior.csv", index=False)
+    LOGGER.info(f"Simulated prior saved at {output_dir}/prior.csv.")
+    return prior
