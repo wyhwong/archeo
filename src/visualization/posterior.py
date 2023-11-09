fd16894e914e986470eb03d10d13fb56e46a13a7
wyhwong <wyhwong@link.cuhk.edu.hk>
2023-11-09 23:49:42 +0800

    Updated kick curve computation

--- src/visualization/posterior.py
+++ src/visualization/posterior.py
@@ -8,7 +8,9 @@ import schemas.visualization
 import visualization.base as base
 
 
-def plot_mass_estimates(df: pd.DataFrame, label: str, output_dir=None, close=True) -> None:
+def plot_mass_estimates(
+    df: pd.DataFrame, label: str, output_dir=None, close=True
+) -> None:
     """
     Plot the distribution of the estimated masses.
 
@@ -28,14 +30,30 @@ def plot_mass_estimates(df: pd.DataFrame, label: str, output_dir=None, close=Tru
     None
     """
     padding = schemas.visualization.Padding(lpad=0.13, bpad=0.14)
-    labels = schemas.visualization.Labels("Distribution of Estimated Masses", "Mass [$M_{\odot}$]", "PDF")
+    labels = schemas.visualization.Labels(
+        "Distribution of Estimated Masses", "Mass [$M_{\odot}$]", "PDF"
+    )
     _, ax = base.initialize_plot(figsize=(9, 4), labels=labels, padding=padding)
-    col_to_labels = {"mf_": f"{label}: ", "m1": "Heavier Parent: ", "m2": "Ligher Parent: "}
+    col_to_labels = {
+        "mf_": f"{label}: ",
+        "m1": "Heavier Parent: ",
+        "m2": "Ligher Parent: ",
+    }
 
     for col, label_prefix in col_to_labels.items():
         density, bins = np.histogram(a=df[col], bins=70, density=True)
-        inv_low, med, inv_high = df[col].quantile(0.05), df[col].quantile(0.5), df[col].quantile(0.95)
-        ax_label = "%s: $%d_{-%d}^{+%d}$ %s" % (label_prefix, med, med - inv_low, inv_high - med, "[$M_{\odot}$]")
+        inv_low, med, inv_high = (
+            df[col].quantile(0.05),
+            df[col].quantile(0.5),
+            df[col].quantile(0.95),
+        )
+        ax_label = "%s: $%d_{-%d}^{+%d}$ %s" % (
+            label_prefix,
+            med,
+            med - inv_low,
+            inv_high - med,
+            "[$M_{\odot}$]",
+        )
         ax.stairs(density, bins, label=ax_label)
 
     plt.ylabel(""), plt.xlabel("")
@@ -43,7 +61,14 @@ def plot_mass_estimates(df: pd.DataFrame, label: str, output_dir=None, close=Tru
     base.savefig_and_close(f"{label}_mass_estimates.png", output_dir, close)
 
 
-def plot_corner(df: pd.DataFrame, label: str, levels=[0.68, 0.9], nbins=70, output_dir=None, close=True) -> None:
+def plot_corner(
+    df: pd.DataFrame,
+    label: str,
+    levels=[0.68, 0.9],
+    nbins=70,
+    output_dir=None,
+    close=True,
+) -> None:
     """
     Plot the posterior corner plot.
 
@@ -85,7 +110,7 @@ def plot_corner(df: pd.DataFrame, label: str, levels=[0.68, 0.9], nbins=70, outp
 
 
 def plot_cumulative_kick_probability_curve(
-    df: pd.DataFrame, label: str, include_pisn=False, output_dir=None, close=True
+    df: pd.DataFrame, label: str, output_dir=None, close=True
 ) -> None:
     """
     Plot the cumulative kick probability curve.
@@ -96,8 +121,6 @@ def plot_cumulative_kick_probability_curve(
         Posterior.
     label : str
         Posterior label.
-    include_pisn : bool, optional
-        Whether to include samples in PISN gap, by default False.
     output_dir : str, optional
         Output directory.
     close : bool, optional
@@ -108,14 +131,20 @@ def plot_cumulative_kick_probability_curve(
     None
     """
     padding = schemas.visualization.Padding(bpad=0.14)
-    labels = schemas.visualization.Labels("Cumulative Kick Probability Curve", "Recoil Velocity $v_f$ ($km/s$)", "CDF")
+    labels = schemas.visualization.Labels(
+        "Cumulative Kick Probability Curve", "Recoil Velocity $v_f$ ($km/s$)", "CDF"
+    )
     _, ax = base.initialize_plot(figsize=(9, 4), labels=labels, padding=padding)
-    if include_pisn:
-        data = df
-    else:
-        data = df.loc[(df["m1"] < 65.0) & (df["m2"] < 65.0)]
-
-    sns.kdeplot(data=data["vf"], cut=0, ax=ax, cumulative=True, label=label)
+    data = df.loc[(df["m1"] < 65) & (df["m2"] < 65)]
+    norm_factor = len(data) / len(df)
+    density, bins = np.histogram(data["vf"], 70, density=True)
+    dbin = bins[1] - bins[0]
+    density *= norm_factor
+    cdf = np.cumsum(density) * dbin
+    x = bins[:-1] + dbin / 2
+    sns.lineplot(y=[0] + list(cdf), x=[0] + list(x), ax=ax, label=label)
     ax.set(ylabel="", xlabel="")
     plt.legend()
-    base.savefig_and_close(f"{label}_cumulative_kick_probability_curve.png", output_dir, close)
+    base.savefig_and_close(
+        f"{label}_cumulative_kick_probability_curve.png", output_dir, close
+    )
