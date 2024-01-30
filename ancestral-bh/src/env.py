bd43e51b14096f45b4f845511b110fae8fc46feb
wyhwong <wyhwong@link.cuhk.edu.hk>
2024-01-31 01:10:27 +0800

    Refactor in progress

--- ancestral-bh/src/env.py
+++ ancestral-bh/src/env.py
@@ -7,6 +7,6 @@ LOGFILE_PATH = os.getenv("LOGFILE_PATH", "./runtime.log")
 # For facade
 RESULTS_DIR = os.getenv("RESULTS_DIR", "../results")
 MAX_MULTIPROCESS_WORKER = int(os.getenv("MAX_MULTIPROCESS_WORKER", "20"))
-CONFIG_PATH = os.getenv("CONFIG_PATH", "../setting.yml")
+CONFIG_PATH = os.getenv("CONFIG_PATH", "../main.yml")
 PRIOR_CONFIG_PATH = os.getenv("PRIOR_CONFIG_PATH", "../prior.yml")
 RANDOM_SEED = int(os.getenv("RANDOM_SEED", "2023"))
