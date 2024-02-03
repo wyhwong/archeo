73853f1e1623820c19103ed7e02a1e3cfe531642
wyhwong <wyhwong@link.cuhk.edu.hk>
2024-02-03 17:42:34 +0800

    Minor fix

--- ancestral-bh/src/core/executor.py
+++ ancestral-bh/src/core/executor.py
@@ -95,7 +95,7 @@ class MultiThreadExecutor(MultiExecutor):
 
         with ThreadPoolExecutor(max_workers=self.max_executors) as executor:
             futures = [executor.submit(func, **kwargs) for kwargs in input_kwargs]
-            tqdm(wait(futures))
+            wait(tqdm(futures))
 
         results = [future.result() for future in futures]
 
