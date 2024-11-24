f313bbaee2b0439c7b7350c50d917123e824e545
wyhwong <wyhwong@link.cuhk.edu.hk>
2024-11-24 23:54:32 +0800

    Minor comments

--- src/archeo/preset/aligned_spin.py
+++ src/archeo/preset/aligned_spin.py
@@ -7,10 +7,10 @@ ALIGNED_SPIN_PRIOR = PriorConfig(
     fits=Fits.NRSUR3DQ8REMNANT,
     is_spin_aligned=True,
     is_only_up_aligned_spin=False,
-    spin=Domain(low=0.0, high=1.0),
-    phi=Domain(low=0.0, high=2.0),
-    theta=Domain(low=0.0, high=1.0),
-    mass=Domain(low=5.0, high=65.0),
-    mass_ratio=Domain(low=1.0, high=6.0),
+    spin=Domain(low=0.0, high=1.0),  # unit: dimensionless
+    phi=Domain(low=0.0, high=2.0),  # unit: pi
+    theta=Domain(low=0.0, high=1.0),  # unit: pi
+    mass=Domain(low=5.0, high=65.0),  # unit: solar mass
+    mass_ratio=Domain(low=1.0, high=6.0),  # unit: dimensionless
     is_mahapatra=False,
 )
--- src/archeo/preset/precessing.py
+++ src/archeo/preset/precessing.py
@@ -7,10 +7,10 @@ PRECESSING_PRIOR = PriorConfig(
     fits=Fits.NRSUR7DQ4REMNANT,
     is_spin_aligned=False,
     is_only_up_aligned_spin=False,
-    spin=Domain(low=0.0, high=1.0),
-    phi=Domain(low=0.0, high=2.0),
-    theta=Domain(low=0.0, high=1.0),
-    mass=Domain(low=5.0, high=65.0),
-    mass_ratio=Domain(low=1.0, high=6.0),
+    spin=Domain(low=0.0, high=1.0),  # unit: dimensionless
+    phi=Domain(low=0.0, high=2.0),  # unit: pi
+    theta=Domain(low=0.0, high=1.0),  # unit: pi
+    mass=Domain(low=5.0, high=65.0),  # unit: solar mass
+    mass_ratio=Domain(low=1.0, high=6.0),  # unit: dimensionless
     is_mahapatra=False,
 )
