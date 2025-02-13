834007cf12d9647c1cab4e6f27d79fe8dff271f8
wyhwong <wyhwong@link.cuhk.edu.hk>
2025-02-13 23:45:06 +0800

    Added Bayesfactor computation

--- src/archeo/core/bayes.py
+++ src/archeo/core/bayes.py
@@ -13,29 +13,61 @@ from archeo.visualization.base import clear_default_labels, initialize_plot
 local_logger = archeo.logger.get_logger(__name__)
 
 
-class PriorReweighter:
-    """A class contains useful methods to reweight posterior samples"""
+class BayesFactorCalculator:
+    """A class to compute the Bayes factor between two priors."""
 
-    def __init__(self, nbins: int = 100, atol: float = 1e-6) -> None:
-        """Initialize the PriorReweighter
+    def __init__(self, nbins: int = 100, atol: float = 1e-6, use_kde: bool = False) -> None:
+        """Initialize the BayesFactorCalculator
 
         Args:
             nbins (int): The number of bins (resolution) for numerical integration
                          or histrogram plotting.
             atol (float): The absolute tolerance
                           for numerical integration of probability distribution.
+            use_kde (bool): Whether to use the KDE method to compute the weights function.
         """
 
         self._nbins = nbins
         self._atol = atol
+        self._use_kde = use_kde
 
         self._lb: Optional[float] = None
         self._ub: Optional[float] = None
         self._binwidth: Optional[float] = None
+        self._bincenters: Optional[np.ndarray] = None
 
-        self.cumulative_bayes_factor: float = 1.0
+    def _setup_bounds(self, *series_collection: pd.Series) -> None:
+        """Setup the bounds for the histograms computation.
+        Suppose the bounds are ONLY valid for one Bayes factor computation.
+        After the computation, the bounds should be reset.
 
-    def _compute_histogram(self, samples: pd.Series) -> np.ndarray:
+        Args:
+            series_collection (pd.Series): The posterior/prior samples of the parameter.
+        """
+
+        if self._lb or self._ub:
+            local_logger.warning("Bounds are initialized. Overwrote the bounds.")
+
+        self._lb: Optional[float] = min(series.min() for series in series_collection)
+        self._ub: Optional[float] = max(series.max() for series in series_collection)
+        self._binwidth = (self._ub - self._lb) / self._nbins
+
+        bin_edges = np.linspace(self._lb, self._ub, self._nbins + 1)
+        self._bincenters = (bin_edges[:-1] + bin_edges[1:]) / 2
+
+        local_logger.info("Bounds: [%.2f, %.2f], Binwidth: %.2f", self._lb, self._ub, self._binwidth)
+
+    def _reset_bounds(self):
+        """Reset the bounds for the histograms computation."""
+
+        self._lb: Optional[float] = None
+        self._ub: Optional[float] = None
+        self._binwidth: Optional[float] = None
+        self._bincenters: Optional[np.ndarray] = None
+
+        local_logger.info("Bounds have been reset.")
+
+    def get_hist(self, samples: pd.Series) -> np.ndarray:
         """Compute the histogram of the samples.
 
         The histogram is used to compute the Bayes factor between two priors.
@@ -48,14 +80,16 @@ class PriorReweighter:
             hist (np.ndarray): The histogram of the samples.
         """
 
-        hist, _ = np.histogram(
-            samples,
-            bins=self._nbins,
-            density=True,
-            range=(self._lb, self._ub),
-        )
-        auc = np.sum(hist) * self._binwidth
+        if self._use_kde:
+            weights_func = gaussian_kde(samples)
+            hist = weights_func(self._bincenters)
+            # Normalize the histogram to ensure the AUC = 1
+            hist /= np.sum(hist) * self._binwidth
 
+        else:
+            hist, _ = np.histogram(samples, bins=self._nbins, density=True, range=(self._lb, self._ub))
+
+        auc = np.sum(hist) * self._binwidth
         if not np.isclose(auc, 1.0, atol=self._atol):
             msg = f"Invalid probability distribution (AUC={auc:.2f})."
             local_logger.error(msg)
@@ -63,56 +97,27 @@ class PriorReweighter:
 
         return hist
 
-    def _setup_bounds(
-        self,
-        new_prior: pd.Series,
-        prior: pd.Series,
-        posterior: pd.Series,
-    ) -> None:
-        """Setup the bounds for the histograms computation.
-        Suppose the bounds are ONLY valid for one Bayes factor computation.
-        After the computation, the bounds should be reset.
-
-        Args:
-            new_prior (pd.Series): The new prior samples of the parameter.
-            prior (pd.Series): The prior samples of the parameter.
-            posterior (pd.Series): The posterior samples of the parameter.
-        """
-
-        if self._lb or self._ub:
-            msg = "Bounds have already been initialized."
-            local_logger.error(msg)
-            raise ValueError(msg)
-
-        self._lb: Optional[float] = min(prior.min(), posterior.min(), new_prior.min())
-        self._ub: Optional[float] = max(prior.max(), posterior.max(), new_prior.max())
-        self._binwidth = (self._ub - self._lb) / self._nbins
-
-        local_logger.info("Bounds: [%.2f, %.2f], Binwidth: %.2f", self._lb, self._ub, self._binwidth)
-
-    def _reset_bounds(self):
-        """Reset the bounds for the histograms computation."""
-
-        self._lb: Optional[float] = None
-        self._ub: Optional[float] = None
-        self._binwidth: Optional[float] = None
-
-        local_logger.info("Bounds have been reset.")
-
-    def get_bayes_factor(self, new_prior: pd.Series, prior: pd.Series, posterior: pd.Series) -> float:
+    def get_bayes_factor(self, candidate_prior: pd.Series, prior: pd.Series, posterior: pd.Series) -> float:
         """Compute the Bayes factor between two models
 
-        The histogram is used to compute the Bayes factor between two priors.
         NOTE: In this implementation, the likelihood function remains untouched.
         So that the Bayes factor is computed as the ratio of the new prior to the old prior.
         Details please check importance sampling.
+
+        Args:
+            candidate_prior (pd.Series): The candidate prior samples of the parameter.
+            prior (pd.Series): The prior samples of the parameter.
+            posterior (pd.Series): The posterior samples of the parameter.
+
+        Returns:
+            bayes_factor (float): The Bayes factor between the candidate prior and the prior.
         """
 
-        self._setup_bounds(new_prior, prior, posterior)
+        self._setup_bounds(candidate_prior, prior, posterior)
 
-        new_prior_hist = self._compute_histogram(new_prior)
-        prior_hist = self._compute_histogram(prior)
-        posterior_hist = self._compute_histogram(posterior)
+        new_prior_hist = self.get_hist(candidate_prior)
+        prior_hist = self.get_hist(prior)
+        posterior_hist = self.get_hist(posterior)
 
         # Original implementation: precision not enough
         # bayes_factor = (new_prior_hist * posterior_hist / prior_hist).sum() * dtheta
@@ -126,11 +131,29 @@ class PriorReweighter:
             np.sum(np.exp(np.log(new_prior_hist[mask]) + np.log(posterior_hist[mask]) - np.log(prior_hist[mask])))
             * self._binwidth
         )
-        self.cumulative_bayes_factor *= bayes_factor
 
         self._reset_bounds()
 
         return bayes_factor
+
+    def get_likelihood_hist(self, prior: pd.Series, posterior: pd.Series) -> tuple[np.ndarray, np.ndarray]:
+        """Compute the likelihood histogram of the parameter."""
+
+        self._setup_bounds(prior, posterior)
+
+        prior_hist = self.get_hist(prior)
+        posterior_hist = self.get_hist(posterior)
+
+        likelihood_hist = posterior_hist / prior_hist
+        # Replace NaNs/infs with zeros
+        likelihood_hist = np.nan_to_num(likelihood_hist, posinf=0.0, neginf=0.0)
+        # normalize the likelihood
+        likelihood_hist /= np.sum(likelihood_hist) * self._binwidth
+        bincenters = self._bincenters
+
+        self._reset_bounds()
+
+        return likelihood_hist, bincenters
 
     def get_weights_func(
         self,
