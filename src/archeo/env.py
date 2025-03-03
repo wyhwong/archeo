440e315f71b618dfc90cf4fb50d3e3a15f329b00
wyhwong <wyhwong@link.cuhk.edu.hk>
2025-03-03 19:40:35 +0800

    Remove default logging filepath

--- src/archeo/env.py
+++ src/archeo/env.py
@@ -5,7 +5,7 @@ import zoneinfo
 # For logging
 STREAMING_LOG_LEVEL = int(os.getenv("STREAMING_LOG_LEVEL", "30"))
 FILE_LOG_LEVEL = int(os.getenv("FILE_LOG_LEVEL", "10"))
-LOG_FILEPATH = os.getenv("LOG_FILEPATH", "./runtime.log")
+LOG_FILEPATH = os.getenv("LOG_FILEPATH")
 LOG_FMT = os.getenv("LOG_FMT", "%(asctime)s [%(name)s | %(levelname)s]: %(message)s")
 LOG_DATEFMT = os.getenv("LOG_DATEFMT", "%Y-%m-%dT%H:%M:%SZ")
 TZ = zoneinfo.ZoneInfo(os.getenv("TZ") or "UTC")
