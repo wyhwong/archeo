b41428871624571a13c9ff12e8b82531e78d2c90
wyhwong <wyhwong@link.cuhk.edu.hk>
2025-06-15 19:16:32 +0800

    Enhance implementation of forward method

---
+++ src/archeo/core/forward/resampler.py
@@ -0,0 +1,264 @@
+from dataclasses import dataclass
+from typing import Optional
+
+import numpy as np
+import pandas as pd
+from scipy.stats import rv_histogram
+
+import archeo.logger
+from archeo.schema import Domain
+from archeo.utils.helper import pre_release
+
+
+local_logger = archeo.logger.get_logger(__name__)
+
+
+def get_histogram(samples: pd.Series, nbins: int, bounds: Domain) -> np.ndarray:
+    """Compute the histogram of the samples"""
+
+    hist, edges = np.histogram(samples, bins=nbins, density=True, range=(bounds.low, bounds.high))
+    binwidth = edges[1] - edges[0]
+
+    auc = np.sum(hist) * binwidth
+    if not np.isclose(auc, 1.0, atol=1e-6):
+        msg = f"Invalid probability distribution (AUC={auc:.2f})."
+        local_logger.warning(msg)
+
+    return hist
+
+
+def _safe_divide(a: np.ndarray, b: np.ndarray, ztol: float = 1e-8) -> np.ndarray:
+    """Safe division to avoid division by zero"""
+
+    return np.where(b > ztol, np.exp(np.log(a) - np.log(b)), 0.0)
+
+
+def get_ratios_rv(a_samples: pd.Series, b_samples: pd.Series, nbins: int, bounds: Domain) -> np.ndarray:
+    """Compute the likelihood of the posterior samples given the prior samples"""
+
+    a_hist = get_histogram(a_samples, nbins, bounds)
+    b_hist = get_histogram(b_samples, nbins, bounds)
+
+    # Avoid division by zero
+    ratio = _safe_divide(a_hist, b_hist)
+
+    rv = rv_histogram((ratio, np.linspace(bounds.low, bounds.high, nbins + 1)))
+    return rv
+
+
+@dataclass
+class ImportanceSamplingData:
+    """Importance sampling data"""
+
+    posterior_samples: pd.DataFrame
+    prior_samples: pd.DataFrame
+    new_prior_samples: pd.DataFrame
+    binsize_spin: float = 0.05
+    binsize_mass: float = 1.0
+
+    def get_binsize(self, col_name: str) -> Optional[float]:
+
+        if col_name.startswith("a"):
+            return self.binsize_spin
+
+        if col_name.startswith("m"):
+            return self.binsize_mass
+
+    def get_nbins(self, col_name: str) -> Optional[int]:
+        """Get the number of bins for a given column name"""
+
+        binsize = self.get_binsize(col_name)
+
+        if binsize is None:
+            return None
+
+        bounds = self.bounds[col_name]
+        return int((bounds.high - bounds.low) / binsize)
+
+    def _get_hist(self, samples: pd.Series) -> np.ndarray:
+        """Get the histogram for a given column name"""
+
+        nbins = self.get_nbins(samples.name)
+
+        if nbins is None:
+            raise ValueError(f"Unknown binsize for column {samples.name}")
+
+        return get_histogram(samples, nbins=nbins, bounds=self.bounds[samples.name])
+
+    def _compute_bounds(self) -> dict[str, Domain]:
+        """Get the bounds of the dataframes"""
+
+        bounds: dict[str, Domain] = {}
+
+        for col in self.posterior_samples.columns:
+            if col not in self.prior_samples.columns:
+                prior_min = np.inf
+                prior_max = -np.inf
+            else:
+                prior_min = self.prior_samples[col].min()
+                prior_max = self.prior_samples[col].max()
+
+            if col not in self.new_prior_samples.columns:
+                new_prior_min = np.inf
+                new_prior_max = -np.inf
+            else:
+                new_prior_min = self.new_prior_samples[col].min()
+                new_prior_max = self.new_prior_samples[col].max()
+
+            _min = min(self.posterior_samples[col].min(), prior_min, new_prior_min)
+            _max = max(self.posterior_samples[col].max(), prior_max, new_prior_max)
+
+            bounds[col] = Domain(low=_min, high=_max)
+
+        return bounds
+
+    def __post_init__(self) -> None:
+        """Compute the bounds of the dataframes"""
+
+        self._bounds = self._compute_bounds()
+
+    @property
+    def bounds(self) -> dict[str, Domain]:
+        """Get the bounds of the dataframes"""
+
+        return self._bounds
+
+    def get_edges(self, col_name: str) -> np.ndarray:
+        """Get the edges of the histogram for a given column name"""
+
+        nbins = self.get_nbins(col_name)
+
+        if nbins is None:
+            raise ValueError(f"Unknown binsize for column {col_name}")
+
+        bounds = self.bounds[col_name]
+        return np.linspace(bounds.low, bounds.high, nbins + 1)
+
+    def get_binwidth(self, col_name: str) -> float:
+        """Get the bin width for a given column name"""
+
+        edges = self.get_edges(col_name)
+        return edges[1] - edges[0]
+
+    @pre_release
+    def get_likelihood_rv(self, col_name: str) -> np.ndarray:
+        """Get the likelihood PDF for a given column name"""
+
+        nbins = self.get_nbins(col_name)
+
+        if nbins is None:
+            raise ValueError(f"Unknown binsize for column {col_name}")
+
+        return get_ratios_rv(
+            self.posterior_samples[col_name],
+            self.prior_samples[col_name],
+            nbins=nbins,
+            bounds=self.bounds[col_name],
+        )
+
+    @pre_release
+    def get_weights(self, col_name: str, ztol=1e-8, new_prior_samples: Optional[pd.Series] = None) -> np.ndarray:
+        """Get the weights for the importance sampling"""
+
+        weights = np.ones(len(self.posterior_samples))
+
+        if (col_name not in self.prior_samples.columns) and (col_name not in self.new_prior_samples.columns):
+            return weights
+
+        prior_hist = self._get_hist(self.prior_samples[col_name]) if col_name in self.new_prior_samples.columns else 1.0
+        if new_prior_samples:
+            new_prior_hist = self._get_hist(new_prior_samples)
+        else:
+            new_prior_hist = (
+                self._get_hist(self.new_prior_samples[col_name]) if col_name in self.new_prior_samples.columns else 1.0
+            )
+        # Avoid division by zero
+        ratio = _safe_divide(new_prior_hist, prior_hist, ztol=ztol)
+        rv = rv_histogram((ratio, self.get_edges(col_name)))
+        weights *= rv.pdf(self.posterior_samples[col_name])
+
+        return weights
+
+    @pre_release
+    def get_bayes_factor(self, ztol=1e-8, random_state=42) -> float:
+        """Compute the Bayes factor between two models
+
+        NOTE: In this implementation, the likelihood function remains untouched.
+        So that the Bayes factor is computed as the ratio of the new prior to the old prior.
+        Details please check importance sampling.
+        """
+
+        bf = 1.0
+        weights = np.ones(len(self.prior_samples))
+
+        for col in self.posterior_samples.columns:
+            if col not in self.new_prior_samples.columns:
+                continue
+
+            prior_hist = self._get_hist(self.prior_samples[col])
+            posterior_hist = self._get_hist(self.posterior_samples[col])
+            new_prior_hist = self._get_hist(self.new_prior_samples[col])
+
+            ratios = _safe_divide(new_prior_hist, prior_hist, ztol=ztol)
+            rv = rv_histogram((ratios, self.get_edges(col_name=col)))
+            weights *= rv.pdf(self.prior_samples[col])
+
+            bf *= np.sum(new_prior_hist * _safe_divide(posterior_hist, prior_hist, ztol=ztol)) * self.get_binwidth(col)
+
+        # Correction
+        inferred_new_prior_samples = self.prior_samples.sample(
+            n=len(self.prior_samples),
+            weights=weights,
+            replace=True,
+            random_state=random_state,
+        )
+        for col in self.posterior_samples:
+            if col in self.new_prior_samples.columns:
+                continue
+
+            prior_hist = self._get_hist(self.prior_samples[col])
+            posterior_hist = self._get_hist(self.posterior_samples[col])
+            new_prior_hist = self._get_hist(inferred_new_prior_samples[col])
+            bf *= np.sum(new_prior_hist * _safe_divide(posterior_hist, prior_hist, ztol=ztol)) * self.get_binwidth(col)
+
+        return bf
+
+    @pre_release
+    def get_reweighted_samples(self, ztol=1e-8, random_state=42) -> pd.DataFrame:
+        """Get the reweighted samples for the importance sampling"""
+
+        weights = np.ones(len(self.posterior_samples))
+
+        for col in self.posterior_samples.columns:
+            if col not in self.new_prior_samples.columns:
+                continue
+            weights *= self.get_weights(col_name=col, ztol=ztol)
+
+        inferred_new_prior_samples = self.prior_samples.sample(
+            n=len(self.prior_samples),
+            weights=weights,
+            replace=True,
+            random_state=random_state,
+        )
+        for col in self.posterior_samples:
+            if col in self.new_prior_samples.columns:
+                continue
+
+            weights *= self.get_weights(
+                col_name=col,
+                ztol=ztol,
+                new_prior_samples=inferred_new_prior_samples[col],
+            )
+
+        reweighted_samples = self.posterior_samples.sample(
+            n=len(self.posterior_samples),
+            weights=weights,
+            replace=True,
+            random_state=random_state,
+        )
+
+        return reweighted_samples
+
+
+# Alias
+ISData = ImportanceSamplingData
--- src/tests/unit-test/test_bayes_factor.py
+++ src/tests/unit-test/test_bayes_factor.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import pytest
 
-from archeo.core.forward.bayes import ISData, get_bayes_factor
+from archeo.core.forward.resampler import ISData
 
 
 np.random.seed(42)
@@ -15,8 +15,8 @@ def default_prior() -> pd.Series:
 
     return pd.DataFrame(
         {
-            "mass": np.random.uniform(low=5, high=65, size=N_SAMPLES),
-            "spin": np.random.uniform(low=0, high=1, size=N_SAMPLES),
+            "m_1": np.random.uniform(low=5, high=65, size=N_SAMPLES),
+            "a_1": np.random.uniform(low=0, high=1, size=N_SAMPLES),
         }
     )
 
@@ -27,19 +27,23 @@ def default_posterior() -> pd.Series:
 
     df = pd.DataFrame(
         {
-            "mass": np.random.normal(loc=35, scale=5, size=N_SAMPLES),
-            "spin": np.random.uniform(low=0, high=1, size=N_SAMPLES),
+            "m_1": np.random.normal(loc=35, scale=5, size=N_SAMPLES),
+            "a_1": np.random.uniform(low=0, high=1, size=N_SAMPLES),
         }
     )
-    return df.clip(lower={"mass": 5, "spin": 0}, upper={"mass": 65, "spin": 1})
+    return df.clip(lower={"m_1": 5, "a_1": 0}, upper={"m_1": 65, "a_1": 1})
 
 
 def test_bayes_factor_with_no_prior_change(prior, posterior):
     """Test the computation of the Bayes factor."""
 
-    candidate_prior = {"mass": np.random.uniform(low=5, high=65, size=N_SAMPLES)}
-    data = ISData(candidate_prior=candidate_prior, posterior=posterior, prior=prior)
-    bayes_factor = get_bayes_factor(data, random_state=42)
+    candidate_prior = pd.DataFrame({"m_1": np.random.uniform(low=5, high=65, size=N_SAMPLES)})
+    data = ISData(
+        new_prior_samples=candidate_prior,
+        posterior_samples=posterior,
+        prior_samples=prior,
+    )
+    bayes_factor = data.get_bayes_factor(random_state=42)
 
     # Here we replace the prior by the original prior
     # The Bayes factor should be exactly 1
@@ -49,9 +53,13 @@ def test_bayes_factor_with_no_prior_change(prior, posterior):
 def test_bayes_factor_replace_delta_prior(prior, posterior):
     """Test the computation of the Bayes factor."""
 
-    candidate_prior = {"mass": np.random.normal(loc=35, scale=0.01, size=N_SAMPLES)}
-    data = ISData(candidate_prior=candidate_prior, posterior=posterior, prior=prior)
-    bayes_factor = get_bayes_factor(data, random_state=42)
+    candidate_prior = pd.DataFrame({"m_1": np.random.normal(loc=35, scale=0.01, size=N_SAMPLES)})
+    data = ISData(
+        new_prior_samples=candidate_prior,
+        posterior_samples=posterior,
+        prior_samples=prior,
+    )
+    bayes_factor = data.get_bayes_factor(random_state=42)
 
     # Expected Bayes factor is 4.73944449
     assert np.isclose(bayes_factor, 4.73944449, atol=0.5)
@@ -62,9 +70,13 @@ def test_bayes_factor_replace_flat_normal_prior(prior, posterior):
 
     samples = np.random.normal(loc=35, scale=50, size=N_SAMPLES)
     samples = samples[(5 <= samples) & (samples <= 65)]
-    candidate_prior = {"mass": samples}
-    data = ISData(candidate_prior=candidate_prior, posterior=posterior, prior=prior)
-    bayes_factor = get_bayes_factor(data, random_state=42)
+    candidate_prior = pd.DataFrame({"m_1": samples})
+    data = ISData(
+        new_prior_samples=candidate_prior,
+        posterior_samples=posterior,
+        prior_samples=prior,
+    )
+    bayes_factor = data.get_bayes_factor(random_state=42)
 
     assert 1.0 <= bayes_factor <= 1.1
 
@@ -72,8 +84,12 @@ def test_bayes_factor_replace_flat_normal_prior(prior, posterior):
 def test_bayes_factor_replace_flat_beta_prior(prior, posterior):
     """Test the computation of the Bayes factor."""
 
-    candidate_prior = {"mass": np.random.beta(a=0.92, b=0.92, size=N_SAMPLES) * 60 + 5}
-    data = ISData(candidate_prior=candidate_prior, posterior=posterior, prior=prior)
-    bayes_factor = get_bayes_factor(data, random_state=42)
+    candidate_prior = pd.DataFrame({"m_1": np.random.beta(a=0.92, b=0.92, size=N_SAMPLES) * 60 + 5})
+    data = ISData(
+        new_prior_samples=candidate_prior,
+        posterior_samples=posterior,
+        prior_samples=prior,
+    )
+    bayes_factor = data.get_bayes_factor(random_state=42)
 
     assert 0.9 <= bayes_factor <= 1.0
