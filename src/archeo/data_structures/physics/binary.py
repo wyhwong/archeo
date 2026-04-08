44a596d06d543386352014a716d9779c4756d014
Henry Wong <wyhwong@AireMacBookNeo.local>
2026-04-08 17:59:44 +0800

    Minor refactoring

--- src/archeo/data_structures/physics/binary.py
+++ src/archeo/data_structures/physics/binary.py
@@ -62,6 +62,8 @@ class BinaryGenerator(BaseModel, frozen=True):
         n_step = 0
 
         while len(binaries) < size:
+            n_step += 1
+
             remaining_size = size - len(binaries)
             primary_black_hole = self.primary_black_hole_source.draw(size=remaining_size)
             secondary_black_hole = self.secondary_black_hole_source.draw(size=remaining_size)
@@ -78,29 +80,36 @@ class BinaryGenerator(BaseModel, frozen=True):
 
                 binaries.append(Binary(primary_black_hole=p_bh, secondary_black_hole=s_bh))
 
-            n_step += 1
             LOGGER.info("Step %d: Generated %d binaries so far.", n_step, len(binaries))
 
+        LOGGER.info("Finished generating %d binaries after %d steps.", size, n_step)
+
         if self.is_aligned_spin:
-            direction_bh1 = np.random.choice([-1, 1], size=size)
-            direction_bh2 = np.random.choice([-1, 1], size=size)
-            binaries = [
-                Binary(
-                    primary_black_hole=BlackHole(
-                        mass=b.primary_black_hole.mass,
-                        spin_magnitude=b.primary_black_hole.spin_magnitude,
-                        spin_vector=(0.0, 0.0, b.primary_black_hole.spin_magnitude * direction_bh1[i]),
-                        speed=b.primary_black_hole.speed,
-                    ),
-                    secondary_black_hole=BlackHole(
-                        mass=b.secondary_black_hole.mass,
-                        spin_magnitude=b.secondary_black_hole.spin_magnitude,
-                        spin_vector=(0.0, 0.0, b.secondary_black_hole.spin_magnitude * direction_bh2[i]),
-                        speed=b.secondary_black_hole.speed,
-                    ),
-                )
-                for i, b in enumerate(binaries)
-            ]
+            binaries = self._apply_aligned_spin_to_binaries(binaries)
             LOGGER.info("Applied aligned spin configuration to the generated binaries.")
 
         return binaries
+
+    def _apply_aligned_spin_to_binaries(self, binaries: Binaries) -> Binaries:
+        """Apply aligned spin configuration to the generated binaries."""
+
+        size = len(binaries)
+        direction_bh1 = np.random.choice([-1, 1], size=size)
+        direction_bh2 = np.random.choice([-1, 1], size=size)
+        return [
+            Binary(
+                primary_black_hole=BlackHole(
+                    mass=b.primary_black_hole.mass,
+                    spin_magnitude=b.primary_black_hole.spin_magnitude,
+                    spin_vector=(0.0, 0.0, b.primary_black_hole.spin_magnitude * direction_bh1[i]),
+                    speed=b.primary_black_hole.speed,
+                ),
+                secondary_black_hole=BlackHole(
+                    mass=b.secondary_black_hole.mass,
+                    spin_magnitude=b.secondary_black_hole.spin_magnitude,
+                    spin_vector=(0.0, 0.0, b.secondary_black_hole.spin_magnitude * direction_bh2[i]),
+                    speed=b.secondary_black_hole.speed,
+                ),
+            )
+            for i, b in enumerate(binaries)
+        ]
