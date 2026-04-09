157e4f8eacbb669a3dc9efa655784878437e73bf
Henry Wong <wyhwong@AireMacBookNeo.local>
2026-04-10 00:33:58 +0800

    Implement binary generator for mass ratio based generation

--- src/archeo/data_structures/math.py
+++ src/archeo/data_structures/math.py
@@ -1,14 +1,32 @@
 from abc import ABC, abstractmethod
+from typing import Union, overload
 
+import numpy as np
 from pydantic import BaseModel
 
 
 class DomainBase(BaseModel, ABC):
     """Base class for domains of parameters."""
 
+    @overload
+    def contains(self, value: float) -> bool: ...
+
+    @overload
+    def contains(self, value: np.ndarray) -> np.ndarray: ...
+
+    @overload
+    def not_contains(self, value: float) -> bool: ...
+
+    @overload
+    def not_contains(self, value: np.ndarray) -> np.ndarray: ...
+
     @abstractmethod
-    def contains(self, value: float) -> bool:
+    def contains(self, value: Union[float, np.ndarray]) -> Union[bool, np.ndarray]:
         """Check if a value is within the domain."""
+
+    @abstractmethod
+    def not_contains(self, value: Union[float, np.ndarray]) -> Union[bool, np.ndarray]:
+        """Check if a value is not within the domain."""
 
 
 class Domain(BaseModel, frozen=True):
@@ -17,10 +35,21 @@ class Domain(BaseModel, frozen=True):
     low: float = float("-inf")
     high: float = float("inf")
 
-    def contains(self, value: float) -> bool:
+    def contains(self, value: Union[float, np.ndarray]) -> Union[bool, np.ndarray]:
         """Check if a value is within the domain."""
 
+        if isinstance(value, np.ndarray):
+            return (value <= self.high) & (value >= self.low)
+
         return self.low <= value <= self.high
+
+    def not_contains(self, value: Union[float, np.ndarray]) -> Union[bool, np.ndarray]:
+        """Check if a value is not within the domain."""
+
+        if isinstance(value, np.ndarray):
+            return (value > self.high) | (value < self.low)
+
+        return not self.contains(value)
 
     def to_tuple(self) -> tuple[float, float]:
         """Convert the domain to a tuple."""
@@ -33,7 +62,18 @@ class PiecewiseDomain(BaseModel, frozen=True):
 
     domains: list[Domain] = []
 
-    def contains(self, value: float) -> bool:
+    def contains(self, value: Union[float, np.ndarray]) -> Union[bool, np.ndarray]:
         """Check if a value is within the piecewise domain."""
 
+        if isinstance(value, np.ndarray):
+            return np.any([domain.contains(value) for domain in self.domains], axis=0)
+
         return any(domain.contains(value) for domain in self.domains)
+
+    def not_contains(self, value: Union[float, np.ndarray]) -> Union[bool, np.ndarray]:
+        """Check if a value is not within the piecewise domain."""
+
+        if isinstance(value, np.ndarray):
+            return np.all([domain.not_contains(value) for domain in self.domains], axis=0)
+
+        return all(domain.not_contains(value) for domain in self.domains)
--- src/archeo/data_structures/physics/binary.py
+++ src/archeo/data_structures/physics/binary.py
@@ -1,10 +1,14 @@
+from abc import abstractmethod
 from typing import TypeAlias
 
 import numpy as np
 from pydantic import BaseModel, NonNegativeFloat, PositiveFloat
 
+from archeo.constants.physics import BH_MASS_LB, PISN_LB
+from archeo.data_structures.annotation import Distribution
+from archeo.data_structures.distribution import Uniform
 from archeo.data_structures.math import Domain
-from archeo.data_structures.physics.black_hole import BlackHole, BlackHoleSource
+from archeo.data_structures.physics.black_hole import BlackHole, BlackHoleGenerator, BlackHoleSource
 from archeo.utils.logger import get_logger
 
 
@@ -46,13 +50,49 @@ class Binary(BaseModel, frozen=True):
 Binaries: TypeAlias = list[Binary]
 
 
-class BinaryGenerator(BaseModel, frozen=True):
+class BinaryGeneratorBase(BaseModel, frozen=True):
+    """Base class for binary generators."""
+
+    is_aligned_spin: bool = False
+
+    @abstractmethod
+    def draw(self, size: int = 1) -> Binaries:
+        """Generate a list of binaries based on the specified sources and mass ratio domain."""
+
+    @staticmethod
+    def _apply_aligned_spin_to_binaries(binaries: Binaries) -> Binaries:
+        """Apply aligned spin configuration to the generated binaries."""
+
+        size = len(binaries)
+        direction_bh1 = np.random.choice([-1, 1], size=size)
+        direction_bh2 = np.random.choice([-1, 1], size=size)
+        binaries = [
+            Binary(
+                primary_black_hole=BlackHole(
+                    mass=b.primary_black_hole.mass,
+                    spin_magnitude=b.primary_black_hole.spin_magnitude,
+                    spin_vector=(0.0, 0.0, b.primary_black_hole.spin_magnitude * direction_bh1[i]),
+                    speed=b.primary_black_hole.speed,
+                ),
+                secondary_black_hole=BlackHole(
+                    mass=b.secondary_black_hole.mass,
+                    spin_magnitude=b.secondary_black_hole.spin_magnitude,
+                    spin_vector=(0.0, 0.0, b.secondary_black_hole.spin_magnitude * direction_bh2[i]),
+                    speed=b.secondary_black_hole.speed,
+                ),
+            )
+            for i, b in enumerate(binaries)
+        ]
+        LOGGER.info("Applied aligned spin configuration to the generated binaries.")
+        return binaries
+
+
+class BinaryGenerator(BinaryGeneratorBase):
     """Binary generator data class."""
 
     primary_black_hole_source: BlackHoleSource
     secondary_black_hole_source: BlackHoleSource
     mass_ratio_domain: Domain = Domain(low=1.0, high=6.0)
-    is_aligned_spin: bool = False
     enforce_source_binding: bool = False
 
     def draw(self, size: int = 1) -> Binaries:
@@ -86,30 +126,59 @@ class BinaryGenerator(BaseModel, frozen=True):
 
         if self.is_aligned_spin:
             binaries = self._apply_aligned_spin_to_binaries(binaries)
-            LOGGER.info("Applied aligned spin configuration to the generated binaries.")
 
         return binaries
 
-    def _apply_aligned_spin_to_binaries(self, binaries: Binaries) -> Binaries:
-        """Apply aligned spin configuration to the generated binaries."""
 
-        size = len(binaries)
-        direction_bh1 = np.random.choice([-1, 1], size=size)
-        direction_bh2 = np.random.choice([-1, 1], size=size)
-        return [
-            Binary(
-                primary_black_hole=BlackHole(
-                    mass=b.primary_black_hole.mass,
-                    spin_magnitude=b.primary_black_hole.spin_magnitude,
-                    spin_vector=(0.0, 0.0, b.primary_black_hole.spin_magnitude * direction_bh1[i]),
-                    speed=b.primary_black_hole.speed,
-                ),
-                secondary_black_hole=BlackHole(
-                    mass=b.secondary_black_hole.mass,
-                    spin_magnitude=b.secondary_black_hole.spin_magnitude,
-                    spin_vector=(0.0, 0.0, b.secondary_black_hole.spin_magnitude * direction_bh2[i]),
-                    speed=b.secondary_black_hole.speed,
-                ),
-            )
-            for i, b in enumerate(binaries)
+class MassRatioBasedBinaryGenerator(BinaryGeneratorBase):
+    """Another option for binary generator that sample binaries based on
+    the mass ratio distribution and primary mass distribution.
+    """
+
+    mass_ratio_distribution: Distribution = Uniform(low=1.0, high=6.0)
+    primary_mass_distribution: Distribution = Uniform(low=BH_MASS_LB, high=PISN_LB)
+    secondary_mass_domain: Domain = Domain(low=BH_MASS_LB, high=PISN_LB)
+    spin_magnitude_distribution: Distribution = Uniform(low=0.0, high=1.0)
+    phi_distribution: Distribution = Uniform(low=0, high=2 * np.pi)
+    theta_distribution: Distribution = Uniform(low=0, high=np.pi)
+
+    def draw(self, size: int = 1) -> Binaries:
+        """Generate a list of binaries based on the specified distributions and mass domain."""
+
+        primary_masses, secondary_masses = self.sample_binary_masses(size)
+        primary_bhs = BlackHoleGenerator.combine_information(
+            masses=primary_masses,
+            spin_magnitudes=self.spin_magnitude_distribution.draw(size),
+            phis=self.phi_distribution.draw(size),
+            thetas=self.theta_distribution.draw(size),
+        )
+        secondary_bhs = BlackHoleGenerator.combine_information(
+            masses=secondary_masses,
+            spin_magnitudes=self.spin_magnitude_distribution.draw(size),
+            phis=self.phi_distribution.draw(size),
+            thetas=self.theta_distribution.draw(size),
+        )
+        binaries = [
+            Binary(primary_black_hole=bh1, secondary_black_hole=bh2) for bh1, bh2 in zip(primary_bhs, secondary_bhs)
         ]
+
+        if self.is_aligned_spin:
+            binaries = self._apply_aligned_spin_to_binaries(binaries)
+
+        return binaries
+
+    def sample_binary_masses(self, size: int) -> tuple[list[float], list[float]]:
+
+        mass_ratios = self.mass_ratio_distribution.draw(size=size)
+        primary_masses = self.primary_mass_distribution.draw(size=size)
+        secondary_masses = primary_masses / mass_ratios
+        n_step = 1
+
+        while (mask := self.secondary_mass_domain.not_contains(secondary_masses)).any():
+            LOGGER.info("Step %d: Generated %d binaries so far.", n_step, len(primary_masses) - mask.sum())
+            primary_masses[mask] = self.primary_mass_distribution.draw(size=mask.sum())
+            secondary_masses[mask] = primary_masses[mask] / mass_ratios[mask]
+            n_step += 1
+
+        LOGGER.info("Finished generating %d binaries after %d steps.", size, n_step)
+        return primary_masses, secondary_masses
